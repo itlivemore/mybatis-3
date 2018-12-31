c18a99ca68532c04711f4971ce4f8608a72eea54
itlivemore <itlivmore@163.com>
2018-12-31 14:19:25 +0800

    初始化部分注释

--- src/main/java/org/apache/ibatis/plugin/Plugin.java
+++ src/main/java/org/apache/ibatis/plugin/Plugin.java
@@ -30,73 +30,74 @@ import org.apache.ibatis.reflection.ExceptionUtil;
  */
 public class Plugin implements InvocationHandler {
 
-  private final Object target;
-  private final Interceptor interceptor;
-  private final Map<Class<?>, Set<Method>> signatureMap;
+	private final Object target;
+	private final Interceptor interceptor;
+	private final Map<Class<?>, Set<Method>> signatureMap;
 
-  private Plugin(Object target, Interceptor interceptor, Map<Class<?>, Set<Method>> signatureMap) {
-    this.target = target;
-    this.interceptor = interceptor;
-    this.signatureMap = signatureMap;
-  }
+	private Plugin(Object target, Interceptor interceptor, Map<Class<?>, Set<Method>> signatureMap) {
+		this.target = target;
+		this.interceptor = interceptor;
+		this.signatureMap = signatureMap;
+	}
 
-  public static Object wrap(Object target, Interceptor interceptor) {
-    Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
-    Class<?> type = target.getClass();
-    Class<?>[] interfaces = getAllInterfaces(type, signatureMap);
-    if (interfaces.length > 0) {
-      return Proxy.newProxyInstance(
-          type.getClassLoader(),
-          interfaces,
-          new Plugin(target, interceptor, signatureMap));
-    }
-    return target;
-  }
+	public static Object wrap(Object target, Interceptor interceptor) {
+		Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
+		Class<?> type = target.getClass();
+		Class<?>[] interfaces = getAllInterfaces(type, signatureMap);
+		// 只有在拦截器里定义拦截的才会创建代理对象
+		if (interfaces.length > 0) {
+			return Proxy.newProxyInstance(type.getClassLoader(), interfaces,
+					new Plugin(target, interceptor, signatureMap));
+		}
+		return target;
+	}
 
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    try {
-      Set<Method> methods = signatureMap.get(method.getDeclaringClass());
-      if (methods != null && methods.contains(method)) {
-        return interceptor.intercept(new Invocation(target, method, args));
-      }
-      return method.invoke(target, args);
-    } catch (Exception e) {
-      throw ExceptionUtil.unwrapThrowable(e);
-    }
-  }
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		try {
+			Set<Method> methods = signatureMap.get(method.getDeclaringClass());
+			if (methods != null && methods.contains(method)) {
+				return interceptor.intercept(new Invocation(target, method, args));
+			}
+			return method.invoke(target, args);
+		} catch (Exception e) {
+			throw ExceptionUtil.unwrapThrowable(e);
+		}
+	}
 
-  private static Map<Class<?>, Set<Method>> getSignatureMap(Interceptor interceptor) {
-    Intercepts interceptsAnnotation = interceptor.getClass().getAnnotation(Intercepts.class);
-    // issue #251
-    if (interceptsAnnotation == null) {
-      throw new PluginException("No @Intercepts annotation was found in interceptor " + interceptor.getClass().getName());      
-    }
-    Signature[] sigs = interceptsAnnotation.value();
-    Map<Class<?>, Set<Method>> signatureMap = new HashMap<>();
-    for (Signature sig : sigs) {
-      Set<Method> methods = signatureMap.computeIfAbsent(sig.type(), k -> new HashSet<>());
-      try {
-        Method method = sig.type().getMethod(sig.method(), sig.args());
-        methods.add(method);
-      } catch (NoSuchMethodException e) {
-        throw new PluginException("Could not find method on " + sig.type() + " named " + sig.method() + ". Cause: " + e, e);
-      }
-    }
-    return signatureMap;
-  }
+	private static Map<Class<?>, Set<Method>> getSignatureMap(Interceptor interceptor) {
+		Intercepts interceptsAnnotation = interceptor.getClass().getAnnotation(Intercepts.class);
+		// issue #251
+		if (interceptsAnnotation == null) {
+			throw new PluginException(
+					"No @Intercepts annotation was found in interceptor " + interceptor.getClass().getName());
+		}
+		Signature[] sigs = interceptsAnnotation.value();
+		Map<Class<?>, Set<Method>> signatureMap = new HashMap<>();
+		for (Signature sig : sigs) {
+			Set<Method> methods = signatureMap.computeIfAbsent(sig.type(), k -> new HashSet<>());
+			try {
+				Method method = sig.type().getMethod(sig.method(), sig.args());
+				methods.add(method);
+			} catch (NoSuchMethodException e) {
+				throw new PluginException(
+						"Could not find method on " + sig.type() + " named " + sig.method() + ". Cause: " + e, e);
+			}
+		}
+		return signatureMap;
+	}
 
-  private static Class<?>[] getAllInterfaces(Class<?> type, Map<Class<?>, Set<Method>> signatureMap) {
-    Set<Class<?>> interfaces = new HashSet<>();
-    while (type != null) {
-      for (Class<?> c : type.getInterfaces()) {
-        if (signatureMap.containsKey(c)) {
-          interfaces.add(c);
-        }
-      }
-      type = type.getSuperclass();
-    }
-    return interfaces.toArray(new Class<?>[interfaces.size()]);
-  }
+	private static Class<?>[] getAllInterfaces(Class<?> type, Map<Class<?>, Set<Method>> signatureMap) {
+		Set<Class<?>> interfaces = new HashSet<>();
+		while (type != null) {
+			for (Class<?> c : type.getInterfaces()) {
+				if (signatureMap.containsKey(c)) {
+					interfaces.add(c);
+				}
+			}
+			type = type.getSuperclass();
+		}
+		return interfaces.toArray(new Class<?>[interfaces.size()]);
+	}
 
 }
--- src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
+++ src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -32,64 +32,67 @@ import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
  */
 public class SqlSessionFactoryBuilder {
 
-  public SqlSessionFactory build(Reader reader) {
-    return build(reader, null, null);
-  }
+	public SqlSessionFactory build(Reader reader) {
+		return build(reader, null, null);
+	}
 
-  public SqlSessionFactory build(Reader reader, String environment) {
-    return build(reader, environment, null);
-  }
+	public SqlSessionFactory build(Reader reader, String environment) {
+		return build(reader, environment, null);
+	}
 
-  public SqlSessionFactory build(Reader reader, Properties properties) {
-    return build(reader, null, properties);
-  }
+	public SqlSessionFactory build(Reader reader, Properties properties) {
+		return build(reader, null, properties);
+	}
 
-  public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
-    try {
-      XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
-      return build(parser.parse());
-    } catch (Exception e) {
-      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
-    } finally {
-      ErrorContext.instance().reset();
-      try {
-        reader.close();
-      } catch (IOException e) {
-        // Intentionally ignore. Prefer previous error.
-      }
-    }
-  }
+	public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
+		try {
+			XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+			return build(parser.parse());
+		} catch (Exception e) {
+			throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+		} finally {
+			ErrorContext.instance().reset();
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// Intentionally ignore. Prefer previous error.
+			}
+		}
+	}
 
-  public SqlSessionFactory build(InputStream inputStream) {
-    return build(inputStream, null, null);
-  }
+	public SqlSessionFactory build(InputStream inputStream) {
+		return build(inputStream, null, null);
+	}
 
-  public SqlSessionFactory build(InputStream inputStream, String environment) {
-    return build(inputStream, environment, null);
-  }
+	public SqlSessionFactory build(InputStream inputStream, String environment) {
+		return build(inputStream, environment, null);
+	}
 
-  public SqlSessionFactory build(InputStream inputStream, Properties properties) {
-    return build(inputStream, null, properties);
-  }
+	public SqlSessionFactory build(InputStream inputStream, Properties properties) {
+		return build(inputStream, null, properties);
+	}
 
-  public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
-    try {
-      XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
-      return build(parser.parse());
-    } catch (Exception e) {
-      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
-    } finally {
-      ErrorContext.instance().reset();
-      try {
-        inputStream.close();
-      } catch (IOException e) {
-        // Intentionally ignore. Prefer previous error.
-      }
-    }
-  }
-    
-  public SqlSessionFactory build(Configuration config) {
-    return new DefaultSqlSessionFactory(config);
-  }
+	public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
+		try {
+			// 创建XMLConfigBuilder用来解析XML配置文件
+			XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
+			// parser.parse()解析生成Configuration对象
+			// build()生成SqlSessionFactory(DefaultSqlSessionFactory)
+			return build(parser.parse());
+		} catch (Exception e) {
+			throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+		} finally {
+			ErrorContext.instance().reset();
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				// Intentionally ignore. Prefer previous error.
+			}
+		}
+	}
+
+	public SqlSessionFactory build(Configuration config) {
+		return new DefaultSqlSessionFactory(config);
+	}
 
 }
